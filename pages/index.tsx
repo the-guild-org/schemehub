99b3d2809820ea35dd858aa0c0a40d9a6972e05c
Laurin Quast <laurinquast@googlemail.com>
2021-07-01 09:00:12 +0200

    remove the flow thing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,13 @@
 import * as Y from "yjs";
 import type * as monaco from "monaco-editor/esm/vs/editor/editor.api";
 import { WebrtcProvider } from "y-webrtc";
-import { useToast, SimpleGrid, Box } from "@chakra-ui/react";
+import { useToast, Box } from "@chakra-ui/react";
 import SchemaEditor from "../components/schema-viewer";
 import { useRouter } from "next/dist/client/router";
 import React from "react";
 import { Page } from "../components/common";
 import { EnrichedLanguageService } from "@theguild/editor";
-import ReactFlow, {
-  ReactFlowProvider,
-  isNode,
-  Node,
-  Position,
-  Connection,
-  Elements,
-  Edge,
-  MiniMap,
-  Controls,
-} from "react-flow-renderer";
-import dagre from "dagre";
-import {
-  buildSchema,
-  GraphQLNamedType,
-  GraphQLOutputType,
-  GraphQLSchema,
-  isInterfaceType,
-  isIntrospectionType,
-  isListType,
-  isNonNullType,
-  isObjectType,
-  isScalarType,
-} from "graphql";
+import { buildSchema, GraphQLSchema } from "graphql";
 import { MonacoBinding } from "../lib/yMonaco";
 import { saveSchema } from "../lib/saveSchema";
 import { fetchSchema } from "../lib/getSchema";
@@ -43,99 +20,6 @@ type Query {
   ping: Int!
 }
 `;
-
-const dagreGraph = new dagre.graphlib.Graph();
-dagreGraph.setDefaultEdgeLabel(() => ({}));
-
-const nodeWidth = 400;
-const nodeHeight = 80;
-
-const getLayoutedElements = (elements: Elements, direction = "TB") => {
-  const isHorizontal = direction === "LR";
-  dagreGraph.setGraph({ rankdir: direction });
-
-  elements.forEach((el: Node | Connection | Edge) => {
-    if (isNode(el)) {
-      dagreGraph.setNode(el.id, { width: nodeWidth, height: nodeHeight });
-    } else {
-      dagreGraph.setEdge(el.source as any, el.target as any);
-    }
-  });
-
-  dagre.layout(dagreGraph);
-
-  return elements.map((el: any) => {
-    if (isNode(el)) {
-      const nodeWithPosition = dagreGraph.node(el.id);
-      el.targetPosition = isHorizontal ? Position.Left : Position.Top;
-      el.sourcePosition = isHorizontal ? Position.Right : Position.Bottom;
-
-      // unfortunately we need this little hack to pass a slightly different position
-      // to notify react flow about the change. Moreover we are shifting the dagre node position
-      // (anchor=center center) to the top left so it matches the react flow node anchor point (top left).
-      el.position = {
-        x: nodeWithPosition.x - nodeWidth / 2 + Math.random() / 1000,
-        y: nodeWithPosition.y - nodeHeight / 2,
-      };
-    }
-
-    return el;
-  });
-};
-
-function unwrapType(t: GraphQLOutputType): GraphQLNamedType {
-  if (isNonNullType(t)) {
-    return unwrapType(t.ofType);
-  }
-
-  if (isListType(t)) {
-    return unwrapType(t.ofType);
-  }
-
-  return t;
-}
-
-function createFromSchema(schema: GraphQLSchema): Elements {
-  const result: Elements = [];
-  const types = schema.getTypeMap();
-
-  for (const [typeName, type] of Object.entries(types)) {
-    if (isScalarType(type) || isIntrospectionType(type)) {
-      continue;
-    }
-
-    result.push({
-      id: typeName,
-      data: {
-        label: typeName,
-      },
-      position: { x: 0, y: 0 },
-    });
-
-    if (isObjectType(type) || isInterfaceType(type)) {
-      const fields = type.getFields();
-
-      for (const [fieldName, field] of Object.entries(fields)) {
-        const baseType = unwrapType(field.type);
-
-        if (isScalarType(baseType) || isIntrospectionType(baseType)) {
-          continue;
-        }
-
-        result.push({
-          id: `${typeName}.${fieldName}`,
-          source: typeName,
-          label: `${fieldName}: ${field.type.toString()}`,
-          target: baseType.name,
-          sourceHandle: `${typeName}.${fieldName}`,
-          animated: true,
-        });
-      }
-    }
-  }
-
-  return getLayoutedElements(result);
-}
 
 const URL_PREFIX = "/#/code/";
 
@@ -196,15 +80,6 @@ export default function Home() {
     }
   }, []);
 
-  const [elements, setElements] = React.useState<Elements>([]);
-
-  React.useEffect(() => {
-    if (languageService && schema) {
-      const elements = createFromSchema(schema);
-      setElements(elements);
-    }
-  }, [schema, languageService]);
-
   const latestSchemaId = React.useRef(schemaId);
   React.useEffect(() => {
     latestSchemaId.current = schemaId;
@@ -250,88 +125,69 @@ export default function Home() {
 
   return (
     <Page>
-      <SimpleGrid columns={2} spacing={4} p={8} background={"black"}>
-        <Box border="1px solid #E535AB" borderRadius={6} overflow={"hidden"}>
-          {initialEditorSchema ? (
-            <React.Suspense fallback={null}>
-              <SchemaEditor
-                editorProps={{
-                  height: "84vh",
-                  theme: "vs-dark",
-                  options: {
-                    automaticLayout: true,
-                    minimap: {
-                      enabled: false,
-                    },
+      <Box overflow={"hidden"}>
+        {initialEditorSchema ? (
+          <React.Suspense fallback={null}>
+            <SchemaEditor
+              editorProps={{
+                height: "84vh",
+                theme: "vs-dark",
+                options: {
+                  automaticLayout: true,
+                  minimap: {
+                    enabled: false,
                   },
-                  sharedLanguageService: languageService,
-                }}
-                schema={initialEditorSchema}
-                onUserSave={async (content) => {
-                  let id = latestSchemaId.current;
-                  if (id == null) {
-                    id = randomHash();
-                    setSchemaId(id);
+                },
+                sharedLanguageService: languageService,
+              }}
+              schema={initialEditorSchema}
+              onUserSave={async (content) => {
+                let id = latestSchemaId.current;
+                if (id == null) {
+                  id = randomHash();
+                  setSchemaId(id);
 
-                    const newURL = `${URL_PREFIX}${id}`;
+                  const newURL = `${URL_PREFIX}${id}`;
 
-                    window.history.replaceState({}, "", newURL);
-                    latestSchemaId.current = id;
-                    connect(
-                      editorInterface.current!.api,
-                      editorInterface.current!.editor
-                    );
-                  }
-                  Promise.all([
-                    saveSchema(id, content),
-                    window.navigator.clipboard.writeText(
-                      location.href.toString()
-                    ),
-                  ]).then(
-                    () => {
-                      toast({
-                        isClosable: true,
-                        position: "bottom",
-                        title: "Sharing link was copied to clipboard",
-                        status: "info",
-                      });
-                    },
-                    (e: any) => alert(e)
+                  window.history.replaceState({}, "", newURL);
+                  latestSchemaId.current = id;
+                  connect(
+                    editorInterface.current!.api,
+                    editorInterface.current!.editor
                   );
-                }}
-                onEditor={(editor, api) => {
-                  editorInterface.current = {
-                    editor,
-                    api,
-                  };
-                  if (latestSchemaId.current) {
-                    setTimeout(() => {
-                      connect(api, editor);
+                }
+                Promise.all([
+                  saveSchema(id, content),
+                  window.navigator.clipboard.writeText(
+                    location.href.toString()
+                  ),
+                ]).then(
+                  () => {
+                    toast({
+                      isClosable: true,
+                      position: "bottom",
+                      title: "Sharing link was copied to clipboard",
+                      status: "info",
                     });
-                  }
-                }}
-              />
-            </React.Suspense>
-          ) : null}
-        </Box>
-        <Box
-          border="1px solid #E535AB"
-          borderRadius={6}
-          overflow={"hidden"}
-          p={4}
-        >
-          <ReactFlowProvider>
-            <ReactFlow
-              elements={elements}
-              nodesDraggable={false}
-              style={{ width: "100%", height: "80vh" }}
-            >
-              <MiniMap />
-              <Controls />
-            </ReactFlow>
-          </ReactFlowProvider>
-        </Box>
-      </SimpleGrid>
+                  },
+                  (e: any) => alert(e)
+                );
+              }}
+              onEditor={(editor, api) => {
+                editorInterface.current = {
+                  editor,
+                  api,
+                };
+                if (latestSchemaId.current) {
+                  setTimeout(() => {
+                    connect(api, editor);
+                  });
+                }
+              }}
+            />
+          </React.Suspense>
+        ) : null}
+      </Box>
     </Page>
   );
 }
